56e1224699af6976e263047862b7baa06c76931e
Erik <eriksantos617@gmail.com>
2020-06-20 23:47:39 -0300

    Atualizacao

---
+++ Estudante.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_Vetor1
+{
+    class Estudante
+    {
+        private string _nome;
+        private string _email;
+
+        public Estudante(string nome, string email)
+        {
+            _nome = nome;
+            _email = email;
+        }
+
+        public string Nome { get => _nome; set => _nome = value; }
+        public string Email { get => _email; set => _email = value; }
+
+        public override string ToString()
+        {
+            return Nome + "," + Email;
+        }
+
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Exercicio_Vetor1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de um vetor da classe estudante de 10 posições.
+            Estudante[] quartos = new Estudante[10];
+            Console.WriteLine("Quantidade de estudantes: ");
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"\nAluguel#{i}: ");
+
+                Console.Write("\nDigite o quarto desejado: ");
+                int numero = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o nome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Digite o E-mail: ");
+                string email = Console.ReadLine();
+
+                //Criação do objeto no vetor de indíce(Quarto) escolhido pelo usuário;
+                quartos[numero] = new Estudante(nome, email);
+                Console.WriteLine("Quarto {0} alugado para {1}", numero, nome);
+
+            }
+
+
+
+
+
+            Console.WriteLine("\nQuartos ocupados: ");
+
+            {
+                for (int i = 0; i < quartos.Length; i++)
+                {
+                    if (quartos[i] != null)
+                    {
+                        Console.WriteLine(i + " " + quartos[i]);
+
+                    }
+                }
+
+
+            }
+        }
+    }
+}
+
+
